5f0e2c252aff0249f2d650e773913a9851498cee
joaolucasgusmao <joaolucasgusmao@hotmail.com>
2025-03-07 17:57:08 -0300

    style: sidemenu created

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,13 +1,23 @@
 "use client";
 
 import { auth } from "@/lib/auth";
+import {
+  Box,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemText,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import PropagateLoader from "react-spinners/PropagateLoader";
 
 export default function DashboardPage() {
   const isAuthenticated = auth();
   const router = useRouter();
+  const [open, setOpen] = useState(false);
 
   useEffect(() => {
     if (isAuthenticated === false) {
@@ -18,16 +28,46 @@ export default function DashboardPage() {
   if (isAuthenticated === null || isAuthenticated === false) {
     return (
       <div className="min-h-screen flex items-center justify-center bg-[var(--background)]">
-        <PropagateLoader color="var(--primary)" />;
+        <PropagateLoader color="var(--primary)" />
       </div>
     );
   }
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-[var(--background)]">
-      <h1 className="text-[var(--primary)] text-2xl font-bold">
-        Bem vindo ao Dashboard!
-      </h1>
-    </div>
+    <>
+      <header className="flex items-center justify-between px-4 bg-[var(--background)] h-14">
+        <IconButton
+          onClick={() => setOpen(true)}
+          sx={{ color: "var(--primary)" }}
+        >
+          <MenuIcon />
+        </IconButton>
+      </header>
+
+      <Drawer
+        anchor="left"
+        open={open}
+        onClose={() => setOpen(false)}
+        sx={{
+          "& .MuiDrawer-paper": {
+            backgroundColor: "var(--background)",
+            color: "var(--primary)",
+          },
+        }}
+      >
+        <Box className="w-64 p-4">
+          <h2 className="text-lg font-bold text-[var(--primary)]">Portal</h2>
+          <List>
+            <ListItem
+              component="button"
+              // onClick={() => router.push("/")}
+              sx={{ color: "var(--primary)" }}
+            >
+              <ListItemText primary="Notícias" />
+            </ListItem>
+          </List>
+        </Box>
+      </Drawer>
+    </>
   );
 }
