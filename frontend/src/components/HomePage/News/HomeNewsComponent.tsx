0584a1c14c71019a89341e3eef15fd45aa6aa7e9
joaolucasgusmao <joaolucasgusmao@hotmail.com>
2025-04-23 17:52:13 -0300

    feat: CoinsComponent implemented

--- frontend/src/components/HomePage/News/HomeNewsComponent.tsx
+++ frontend/src/components/HomePage/News/HomeNewsComponent.tsx
@@ -2,9 +2,7 @@ import { ICoins } from "@/@types/coins";
 import { INewsReturn } from "@/@types/news";
 import { useState } from "react";
 
-import ExpandLessIcon from "@mui/icons-material/ExpandLess";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import CoinsComponent from "../commons/CoinsComponent";
 
 interface HomeNewsComponentProps {
   news: INewsReturn[];
@@ -13,6 +11,7 @@ interface HomeNewsComponentProps {
 
 const HomeNewsComponent = ({ news, coins }: HomeNewsComponentProps) => {
   const [maxCharsSummary] = useState<number>(107);
+  const [maxCharsTitle] = useState<number>(80);
   const homeNews = news.filter((newsItem) => newsItem.is_active).slice(0, 30);
 
   return (
@@ -38,9 +37,11 @@ const HomeNewsComponent = ({ news, coins }: HomeNewsComponentProps) => {
                   {news.categories.map((category) => category.name).join(", ")}
                 </span>
               </div>
-              <div className="flex flex-col gap-2">
+              <div className="flex flex-col gap-4">
                 <h1 className="text-xl font-bold text-[var(--orange)] lg:text-2xl">
-                  {news.title}
+                  {news.title.length > maxCharsTitle
+                    ? `${news.title.slice(0, maxCharsTitle)}...`
+                    : news.title}
                 </h1>
                 {news.summary && (
                   <p className="text-sm text-[var(--gray-3)] lg:text-base">
@@ -54,209 +55,7 @@ const HomeNewsComponent = ({ news, coins }: HomeNewsComponentProps) => {
           </div>
         ))}
       </div>
-      <div className="hidden xl:flex xl:flex-col">
-        <h1 className="font-bold text-lg text-[var(--black)] mb-2 text-center">
-          Economia
-        </h1>
-        <div className="pt-2 w-[350px] h-[225px] border border-[var(--gray-2)]">
-          <div className="flex items-center gap-16 justify-center">
-            <div className="flex flex-col">
-              <h2 className="text-base font-medium text-[var(--black)] text-center">
-                Dólar
-              </h2>
-              <p className="text-[0.900rem] font-bold text-[var(--black)] pl-2">
-                {Number(coins.USDBRL.ask).toLocaleString("pt-BR", {
-                  style: "currency",
-                  currency: "BRL",
-                })}
-              </p>
-              <p
-                className={`text-sm font-bold ${
-                  Number(coins.USDBRL.pctChange) > 0
-                    ? "text-[var(--green)]"
-                    : Number(coins.USDBRL.pctChange) < 0
-                    ? "text-[var(--red)]"
-                    : "text-[var(--gray-3)]"
-                }`}
-              >
-                {coins.USDBRL.pctChange &&
-                Number(coins.USDBRL.pctChange) > 0 ? (
-                  <>
-                    <ExpandLessIcon
-                      style={{
-                        color: "text-[var(--green)]",
-                        fontSize: "20px",
-                      }}
-                    />
-                    +
-                    {String(Number(coins.USDBRL.pctChange).toFixed(2)).replace(
-                      ".",
-                      ","
-                    )}
-                  </>
-                ) : coins.USDBRL.pctChange &&
-                  Number(coins.USDBRL.pctChange) < 0 ? (
-                  <>
-                    <ExpandMoreIcon
-                      style={{
-                        color: "text-[var(--red)]",
-                        fontSize: "20px",
-                      }}
-                    />
-                    {String(Number(coins.USDBRL.pctChange).toFixed(2)).replace(
-                      ".",
-                      ","
-                    )}
-                  </>
-                ) : (
-                  <>
-                    <ChevronRightIcon
-                      style={{
-                        color: "text-[var(--gray)]",
-                        fontSize: "20px",
-                      }}
-                    />
-                    {String(Number(coins.USDBRL.pctChange).toFixed(2)).replace(
-                      ".",
-                      ","
-                    )}
-                  </>
-                )}
-                %
-              </p>
-            </div>
-            <div className="w-[1px] h-[100px] bg-[var(--gray-2)] rotate-45" />
-
-            <div className="flex flex-col">
-              <h2 className="text-base font-medium text-[var(--black)] text-center">
-                Euro
-              </h2>
-              <p className="text-[0.900rem] font-bold text-[var(--black)] pl-2">
-                {Number(coins.EURBRL.ask).toLocaleString("pt-BR", {
-                  style: "currency",
-                  currency: "BRL",
-                })}
-              </p>
-              <p
-                className={`text-sm font-bold ${
-                  Number(coins.EURBRL.pctChange) > 0
-                    ? "text-[var(--green)]"
-                    : Number(coins.EURBRL.pctChange) < 0
-                    ? "text-[var(--red)]"
-                    : "text-[var(--gray-3)]"
-                }`}
-              >
-                {coins.EURBRL.pctChange &&
-                Number(coins.EURBRL.pctChange) > 0 ? (
-                  <>
-                    <ExpandLessIcon
-                      style={{
-                        color: "text-[var(--green)]",
-                        fontSize: "20px",
-                      }}
-                    />
-                    +
-                    {String(Number(coins.EURBRL.pctChange).toFixed(2)).replace(
-                      ".",
-                      ","
-                    )}
-                  </>
-                ) : coins.EURBRL.pctChange &&
-                  Number(coins.EURBRL.pctChange) < 0 ? (
-                  <>
-                    <ExpandMoreIcon
-                      style={{
-                        color: "text-[var(--red)]",
-                        fontSize: "20px",
-                      }}
-                    />
-                    {String(Number(coins.EURBRL.pctChange).toFixed(2)).replace(
-                      ".",
-                      ","
-                    )}
-                  </>
-                ) : (
-                  <>
-                    <ChevronRightIcon
-                      style={{
-                        color: "text-[var(--gray-3)]",
-                        fontSize: "20px",
-                      }}
-                    />
-                    {String(Number(coins.EURBRL.pctChange).toFixed(2)).replace(
-                      ".",
-                      ","
-                    )}
-                  </>
-                )}
-                %
-              </p>
-            </div>
-          </div>
-          <div className="flex items-center justify-center flex-col mt-1 pt-6 border-t border-[var(--gray-2)]">
-            <h2 className="text-base font-bold text-[var(--black)]">Bitcoin</h2>
-            <p className="text-[0.900rem] font-bold text-[var(--black)]">
-              {Number(coins.BTCBRL.ask).toLocaleString("pt-BR", {
-                style: "currency",
-                currency: "BRL",
-              })}
-            </p>
-            <p
-              className={`text-sm font-bold ${
-                Number(coins.BTCBRL.pctChange) > 0
-                  ? "text-[var(--green)]"
-                  : Number(coins.BTCBRL.pctChange) < 0
-                  ? "text-[var(--red)]"
-                  : "text-[var(--gray-3)]"
-              }`}
-            >
-              {coins.BTCBRL.pctChange && Number(coins.BTCBRL.pctChange) > 0 ? (
-                <>
-                  <ExpandLessIcon
-                    style={{
-                      color: "text-[var(--green)]",
-                      fontSize: "20px",
-                    }}
-                  />
-                  +
-                  {String(Number(coins.BTCBRL.pctChange).toFixed(2)).replace(
-                    ".",
-                    ","
-                  )}
-                </>
-              ) : coins.BTCBRL.pctChange &&
-                Number(coins.BTCBRL.pctChange) < 0 ? (
-                <>
-                  <ExpandMoreIcon
-                    style={{
-                      color: "text-[var(--red)]",
-                      fontSize: "20px",
-                    }}
-                  />
-                  {String(Number(coins.BTCBRL.pctChange).toFixed(2)).replace(
-                    ".",
-                    ","
-                  )}
-                </>
-              ) : (
-                <>
-                  <ChevronRightIcon
-                    style={{
-                      color: "text-[var(--gray-3)]",
-                      fontSize: "20px",
-                    }}
-                  />
-                  {String(Number(coins.BTCBRL.pctChange).toFixed(2)).replace(
-                    ".",
-                    ","
-                  )}
-                </>
-              )}
-              %
-            </p>
-          </div>
-        </div>
-      </div>
+      <CoinsComponent coins={coins} />
     </div>
   );
 };
---
+++ frontend/src/components/HomePage/commons/CoinsComponent.tsx
@@ -0,0 +1,215 @@
+import ExpandLessIcon from "@mui/icons-material/ExpandLess";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import { ICoins } from "@/@types/coins";
+
+interface CoinsComponentProps {
+  coins: ICoins;
+}
+
+const CoinsComponent = ({ coins }: CoinsComponentProps) => {
+  return (
+    <div className="hidden xl:flex xl:flex-col">
+      <h1 className="font-bold text-lg text-[var(--black)] mb-2 text-center">
+        Economia
+      </h1>
+      <div className="pt-2 w-[350px] h-[225px] border border-[var(--gray-2)]">
+        <div className="flex items-center gap-16 justify-center">
+          <div className="flex flex-col">
+            <h2 className="text-base font-medium text-[var(--black)] text-center">
+              Dólar
+            </h2>
+            <p className="text-[0.900rem] font-bold text-[var(--black)] pl-2">
+              {Number(coins.USDBRL.ask).toLocaleString("pt-BR", {
+                style: "currency",
+                currency: "BRL",
+              })}
+            </p>
+            <p
+              className={`text-sm font-bold ${
+                Number(coins.USDBRL.pctChange) > 0
+                  ? "text-[var(--green)]"
+                  : Number(coins.USDBRL.pctChange) < 0
+                  ? "text-[var(--red)]"
+                  : "text-[var(--gray-3)]"
+              }`}
+            >
+              {coins.USDBRL.pctChange && Number(coins.USDBRL.pctChange) > 0 ? (
+                <>
+                  <ExpandLessIcon
+                    style={{
+                      color: "text-[var(--green)]",
+                      fontSize: "20px",
+                    }}
+                  />
+                  +
+                  {String(Number(coins.USDBRL.pctChange).toFixed(2)).replace(
+                    ".",
+                    ","
+                  )}
+                </>
+              ) : coins.USDBRL.pctChange &&
+                Number(coins.USDBRL.pctChange) < 0 ? (
+                <>
+                  <ExpandMoreIcon
+                    style={{
+                      color: "text-[var(--red)]",
+                      fontSize: "20px",
+                    }}
+                  />
+                  {String(Number(coins.USDBRL.pctChange).toFixed(2)).replace(
+                    ".",
+                    ","
+                  )}
+                </>
+              ) : (
+                <>
+                  <ChevronRightIcon
+                    style={{
+                      color: "text-[var(--gray)]",
+                      fontSize: "20px",
+                    }}
+                  />
+                  {String(Number(coins.USDBRL.pctChange).toFixed(2)).replace(
+                    ".",
+                    ","
+                  )}
+                </>
+              )}
+              %
+            </p>
+          </div>
+          <div className="w-[1px] h-[100px] bg-[var(--gray-2)] rotate-45" />
+
+          <div className="flex flex-col">
+            <h2 className="text-base font-medium text-[var(--black)] text-center">
+              Euro
+            </h2>
+            <p className="text-[0.900rem] font-bold text-[var(--black)] pl-2">
+              {Number(coins.EURBRL.ask).toLocaleString("pt-BR", {
+                style: "currency",
+                currency: "BRL",
+              })}
+            </p>
+            <p
+              className={`text-sm font-bold ${
+                Number(coins.EURBRL.pctChange) > 0
+                  ? "text-[var(--green)]"
+                  : Number(coins.EURBRL.pctChange) < 0
+                  ? "text-[var(--red)]"
+                  : "text-[var(--gray-3)]"
+              }`}
+            >
+              {coins.EURBRL.pctChange && Number(coins.EURBRL.pctChange) > 0 ? (
+                <>
+                  <ExpandLessIcon
+                    style={{
+                      color: "text-[var(--green)]",
+                      fontSize: "20px",
+                    }}
+                  />
+                  +
+                  {String(Number(coins.EURBRL.pctChange).toFixed(2)).replace(
+                    ".",
+                    ","
+                  )}
+                </>
+              ) : coins.EURBRL.pctChange &&
+                Number(coins.EURBRL.pctChange) < 0 ? (
+                <>
+                  <ExpandMoreIcon
+                    style={{
+                      color: "text-[var(--red)]",
+                      fontSize: "20px",
+                    }}
+                  />
+                  {String(Number(coins.EURBRL.pctChange).toFixed(2)).replace(
+                    ".",
+                    ","
+                  )}
+                </>
+              ) : (
+                <>
+                  <ChevronRightIcon
+                    style={{
+                      color: "text-[var(--gray-3)]",
+                      fontSize: "20px",
+                    }}
+                  />
+                  {String(Number(coins.EURBRL.pctChange).toFixed(2)).replace(
+                    ".",
+                    ","
+                  )}
+                </>
+              )}
+              %
+            </p>
+          </div>
+        </div>
+        <div className="flex items-center justify-center flex-col mt-1 pt-6 border-t border-[var(--gray-2)]">
+          <h2 className="text-base font-bold text-[var(--black)]">Bitcoin</h2>
+          <p className="text-[0.900rem] font-bold text-[var(--black)]">
+            {Number(coins.BTCBRL.ask).toLocaleString("pt-BR", {
+              style: "currency",
+              currency: "BRL",
+            })}
+          </p>
+          <p
+            className={`text-sm font-bold ${
+              Number(coins.BTCBRL.pctChange) > 0
+                ? "text-[var(--green)]"
+                : Number(coins.BTCBRL.pctChange) < 0
+                ? "text-[var(--red)]"
+                : "text-[var(--gray-3)]"
+            }`}
+          >
+            {coins.BTCBRL.pctChange && Number(coins.BTCBRL.pctChange) > 0 ? (
+              <>
+                <ExpandLessIcon
+                  style={{
+                    color: "text-[var(--green)]",
+                    fontSize: "20px",
+                  }}
+                />
+                +
+                {String(Number(coins.BTCBRL.pctChange).toFixed(2)).replace(
+                  ".",
+                  ","
+                )}
+              </>
+            ) : coins.BTCBRL.pctChange && Number(coins.BTCBRL.pctChange) < 0 ? (
+              <>
+                <ExpandMoreIcon
+                  style={{
+                    color: "text-[var(--red)]",
+                    fontSize: "20px",
+                  }}
+                />
+                {String(Number(coins.BTCBRL.pctChange).toFixed(2)).replace(
+                  ".",
+                  ","
+                )}
+              </>
+            ) : (
+              <>
+                <ChevronRightIcon
+                  style={{
+                    color: "text-[var(--gray-3)]",
+                    fontSize: "20px",
+                  }}
+                />
+                {String(Number(coins.BTCBRL.pctChange).toFixed(2)).replace(
+                  ".",
+                  ","
+                )}
+              </>
+            )}
+            %
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CoinsComponent;
